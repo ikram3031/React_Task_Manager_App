36e78ebc2908841966c9965df96904c440630261
ikram3031 <mdikram.3031@gmail.com>
2023-03-30 15:50:34 +0600

    2nd

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,8 +1,24 @@
-import React from 'react'
+import React from 'react';
+import { useNavigate } from 'react-router-dom';
+import './Home.scss';
 
 const Home = () => {
+    const navigate = useNavigate();
+
     return (
-        <div>Home</div>
+        <div className='home'>
+            <h2>Welcome to Task Manager App</h2>
+
+            <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                Fugit et reprehenderit!
+            </p>
+
+            <div className="buttons">
+                <button onClick={() => navigate('/addtask')} className='btn'>Add Task</button>
+                <button onClick={() => navigate('/addemployee')} className='btn'>Add Employee</button>
+            </div>
+        </div>
     )
 }
 
